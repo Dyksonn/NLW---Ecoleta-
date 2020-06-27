acf20051f2f5e45042945d728bcc943ea13a1bf1
dykson <dykson@smit.net.br>
2020-06-27 17:20:32 -0300

    Back-end da aplicação feito

---
+++ Back-End/src/controllers/ItemsContoller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+import knex from '../database/connection'
+
+class ItemsController {
+    async index (req: Request, res: Response) {
+        const items = await knex('items').select('*')
+    
+        const serializedItems = items.map((items) => {
+            return {
+                id: items.id,
+                title: items.title,
+                image_url: `http://localhost:3333/uploads/${items.image}`
+            }
+        })
+    
+        return res.json(serializedItems);
+    }
+}
+
+export default ItemsController
